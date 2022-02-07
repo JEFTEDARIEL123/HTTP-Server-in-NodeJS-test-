3b8e0c6b7cfaf81d86bf52b0d0890d8184ac2030
Jefté Dariel Mendoza Castro <63319744+JEFTEDARIEL123@users.noreply.github.com>
2022-02-07 17:17:52 -0600

    Logs updated

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 var express = require('express')
 var colors = require('colors')
-var app = express()
+var morgan = require('morgan')
 require('dotenv').config();
 
+   console.log('Starting app...')
+
+var app = express()
+
+   console.log('Loading web files from /www/')
+
 app.use(express.static('www'));
+
+   console.log('Loading Config from .env')
 
 var server = app.listen(process.env.PORT, function () {
 
     var host = server.address().address
     var port = server.address().port
 
+    app.use(morgan('common'))
 
-
-
+    console.log(' ')
     console.log('#############################'.yellow)
     console.log('#     WebServer Online!     #'.yellow)
     console.log('# Insert ur files in ./www/ #'.yellow)
     console.log('#############################'.yellow)
     console.log(' ')
-    console.log('Address And Port:'.green, process.env.ADDRESS, '@'.green, process.env.PORT)
+    console.log('Server running on:'.green, process.env.ADDRESS, '@'.green, process.env.PORT)
     console.log(' ')
+
+    
+    
 })
